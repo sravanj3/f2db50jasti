0d2f8a63734c3d1feee98816212941366763fa8d
Sravan Satish Jasti <sravansatish2000@gmail.com>
2022-11-30 22:31:27 -0600

    created login file

--- routes/jungle.js
+++ routes/jungle.js
@@ -12,6 +12,17 @@ var express = require('express');
 const jungle_controllers= require('../controllers/jungle'); 
 var router = express.Router(); 
  
+// A little function to check if we have an authorized user and continue on 
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+ }
+
 /* GET jungle */ 
 router.get('/', jungle_controllers.jungle_view_all_Page ); 
 module.exports = router; 
@@ -20,19 +31,13 @@ module.exports = router;
 router.get('/jungle/:id', jungle_controllers.jungle_detail); 
 
 /* GET detail jungle page */ 
-router.get('/detail', jungle_controllers.jungle_view_one_Page); 
+router.get('/detail',secured, jungle_controllers.jungle_view_one_Page); 
 
 /* GET create jungle page */ 
-router.get('/create', jungle_controllers.jungle_create_Page); 
+router.get('/create',secured, jungle_controllers.jungle_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', jungle_controllers.jungle_update_Page); 
+router.get('/update',secured, jungle_controllers.jungle_update_Page); 
 
 /* GET delete jungle page */ 
-router.get('/delete', jungle_controllers.jungle_delete_Page);
-
-/* GET create jungle page */
-router.get('/create', jungle_controllers.jungle_create_Page);
-
-/* GET create update page */
-router.get('/update', jungle_controllers.jungle_update_Page);
+router.get('/delete',secured, jungle_controllers.jungle_delete_Page);
