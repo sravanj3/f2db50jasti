9ab8ff080e977f7723f142f08c79c8b3dee7c1ea
Sravan Satish Jasti <sravansatish2000@gmail.com>
2022-11-30 23:21:55 -0600

    updated models

--- models/jungle.js
+++ models/jungle.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose")
 const jungleSchema = mongoose.Schema({
-animal_name: String,
-animal_type: String,
-animal_quantity: Number
+animal_name: {
+    type:String,
+    required:true
+},
+animal_type: {
+    type:String,
+    required:true
+},
+animal_quantity: {
+    type:Number,
+    required:true,
+    min:0,
+    max:100
+}
 })
 
 module.exports = mongoose.model("jungle", jungleSchema)
